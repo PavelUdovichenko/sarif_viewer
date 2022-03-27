781883a3c2672e2485d07140763ea077543cfc35
SaintPavl <Udovichenkop@gmail.com>
2022-03-27 23:32:15 +0300

    Update PopupDialogAction.java

--- src/main/java/org/source/sarif_viewer/actions/PopupDialogAction.java
+++ src/main/java/org/source/sarif_viewer/actions/PopupDialogAction.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+//asda
 package org.source.sarif_viewer.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
