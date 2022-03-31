d4706fb9115af24f0c79d22b89d463a83ebe8f55
andre <andrei.kalinin.99@yandex.ru>
2022-04-01 01:00:50 +0300

    add icon

---
+++ src/main/java/icons/PluginIcons.java
@@ -0,0 +1,8 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+import javax.swing.*;
+
+public interface PluginIcons {
+    Icon EDIT_LOCATION_ICON = IconLoader.getIcon("/icons/logo.png");
+}
