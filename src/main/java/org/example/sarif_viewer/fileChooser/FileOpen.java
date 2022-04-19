daa0ae2f0b13c9655a9f538fcefc11c52b6eeb02
PavelUdovichenko <Udovichenkop@gmail.com>
2022-04-19 04:58:14 +0300

    try to psi

--- src/main/java/org/example/sarif_viewer/fileChooser/FileOpen.java
+++ src/main/java/org/example/sarif_viewer/fileChooser/FileOpen.java
@@ -1,20 +1,27 @@
 package org.example.sarif_viewer.fileChooser;
 
+import com.intellij.psi.PsiFile;
+
 import javax.swing.*;
+import java.io.File;
 
 public class FileOpen {
     public static String pathFile = "";
     public static String openFile = "";
+    private static PsiFile containingFile;
+    public static File gFile;
 
     public static void clickBtn() {
         JFileChooser jFileChooser = new JFileChooser();
-
+        jFileChooser.setDragEnabled(true);
+        jFileChooser.setMultiSelectionEnabled(true);
         jFileChooser.setDialogTitle("Open file");
 
         // Определяем фильтры типов файлов
         FileFilterExt ff = new FileFilterExt("sarif", "SARIF-Files (*.sarif)");
         jFileChooser.setAcceptAllFileFilterUsed(false);
         jFileChooser.setFileFilter(ff);
+
 
         // Определение режима - только файл
         jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -25,6 +32,11 @@ public class FileOpen {
         if (result == JFileChooser.APPROVE_OPTION ) {
             pathFile = jFileChooser.getSelectedFile().toString();
             openFile = jFileChooser.getSelectedFile().getName();
+            gFile = jFileChooser.getSelectedFile();
         }
     }
+
+
+
+
 }
